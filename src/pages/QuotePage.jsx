5e71ce7675a94b8709d3908d71f133ec130485ba
Trần Linh Nhi <20521719@ms.uit.edu.vn>
2024-03-26 10:37:27 +0700

    setup

---
+++ src/pages/QuotePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from '../components/Footer';
+
+const QuotePage = () => {
+  return (
+    <div className="container">
+      Quote Page
+      <Footer />
+    </div>
+  );
+};
+
+export default QuotePage;
